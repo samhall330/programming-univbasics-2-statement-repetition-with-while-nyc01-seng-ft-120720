ac765c7c3b16fc8daa98aad21b228c1d6c279805
Sam Hall <mann.samantha11@gmail.com>
2020-11-15 03:53:32 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -3,3 +3,5 @@ while count >= 1 do # A Boolean expression using the bit of data
   puts count # Work
   count -= 1 # A bit of work that moves the bit of data closer to being false
 end
+
+puts "Happy New Year!"
